22895a5032a52bb13b1070307a738f8abb1eb501
Wevioz <wevioz@mail.ru>
2022-04-06 07:54:36 +0300

    Done the courses Array

---
+++ js/script.js
@@ -0,0 +1,247 @@
+const courseCategoryes = {
+    javaScript: {
+        name : "JavaScirpt"
+    },
+    wordPress: {
+        name : "WordPress"
+    },
+    react: {
+        name : "React"
+    },
+    php: {
+        name : "PHP"
+    },
+    python: {
+        name : "Python"
+    },
+    css: {
+        name : "CSS"
+    },
+    html5: {
+        name : "HTML5"
+    },
+    nodeJs: {
+        name : "Node.JS"
+    },
+    angular: {
+        name : "Angular"
+    },
+    vueJs: {
+        name : "Vue JS"
+    },
+    django: {
+        name : "Django"
+    },
+    reduxFramework: {
+        name : "Redus Framework"
+    },
+    linux: {
+        name : "Linux"
+    },
+    bootstrap: {
+        name : "Bootstrap"
+    }
+}
+
+const coursesData = [
+    {
+        title : "Полный курс по JavaScript + React - с нуля до результата",
+        category : courseCategoryes.javaScript,
+        duration : 58,
+        rating : 4.8,
+        image : "img/picture-1.svg"
+    },
+    
+    {
+        title : "Современный JavaScript - с Нуля до Junior Специалиста",
+        category : courseCategoryes.javaScript,
+        duration : 66,
+        rating : 4.6,
+        image : "img/picture-2.svg"
+    },
+    
+    {
+        title : "Посадка верстки и создание тем на CMS WordPress",
+        category : courseCategoryes.wordPress,
+        duration : 6,
+        rating : 4.8,
+        image : "img/picture-3.svg"
+    },
+    
+    {
+        title : "Создание сайтов без программирования на WordPress",
+        category : courseCategoryes.wordPress,
+        duration : 15,
+        rating : 4.5,
+        image : "img/picture-4.svg"
+    },
+    
+    {
+        title : "React с нуля для начинающих",
+        category : courseCategoryes.react,
+        duration : 13,
+        rating : 4.8,
+        image : "img/picture-5.svg"
+    },
+    
+    {
+        title : "React + Redux",
+        category : courseCategoryes.react,
+        duration : 22,
+        rating : 4.6,
+        image : "img/picture-6.svg"
+    },
+    
+    {
+        title : "PHP с нуля",
+        category : courseCategoryes.php,
+        duration : 43,
+        rating : 3.4,
+        image : "img/picture-1.svg"
+    },
+    
+    {
+        title : "Python с нуля",
+        category : courseCategoryes.python,
+        duration : 23,
+        rating : 4.5,
+        image : "img/picture-2.svg"
+    },
+    
+    {
+        title : "CSS с нуля",
+        category : courseCategoryes.css,
+        duration : 56,
+        rating : 5.0,
+        image : "img/picture-3.svg"
+    },
+    
+    {
+        title : "HTML5 с нуля",
+        category : courseCategoryes.html5,
+        duration : 34,
+        rating : 2.9,
+        image : "img/picture-4.svg"
+    },
+    
+    {
+        title : "Node.JS с нуля",
+        category : courseCategoryes.nodeJs,
+        duration : 47,
+        rating : 4.1,
+        image : "img/picture-5.svg"
+    },
+    
+    {
+        title : "Angular с нуля",
+        category : courseCategoryes.angular,
+        duration : 32,
+        rating : 4.5,
+        image : "img/picture-6.svg"
+    },
+    
+    {
+        title : "Vue JS с нуля",
+        category : courseCategoryes.vueJs,
+        duration : 90,
+        rating : 3.9,
+        image : "img/picture-1.svg"
+    },
+    
+    {
+        title : "Django с нуля",
+        category : courseCategoryes.django,
+        duration : 46,
+        rating : 2.1,
+        image : "img/picture-2.svg"
+    },
+    
+    {
+        title : "Redux Framework с нуля",
+        category : courseCategoryes.reduxFramework,
+        duration : 90,
+        rating : 4.8,
+        image : "img/picture-3.svg"
+    },
+    
+    {
+        title : "Linux с нуля",
+        category : courseCategoryes.linux,
+        duration : 34,
+        rating : 4.3,
+        image : "img/picture-4.svg"
+    },
+    
+    {
+        title : "Bootstrap с нуля",
+        category : courseCategoryes.bootstrap,
+        duration : 65,
+        rating : 1.9,
+        image : "img/picture-5.svg"
+    },
+    
+    {
+        title : "PHP для продвинутых",
+        category : courseCategoryes.php,
+        duration : 67,
+        rating : 3.2,
+        image : "img/picture-6.svg"
+    },
+    
+    {
+        title : "Python для продвинутых",
+        category : courseCategoryes.python,
+        duration : 87,
+        rating : 4.1,
+        image : "img/picture-1.svg"
+    },
+    
+    {
+        title : "CSS для продвинутых",
+        category : courseCategoryes.css,
+        duration : 45,
+        rating : 3.7,
+        image : "img/picture-2.svg"
+    },
+    
+    {
+        title : "HTML5 для продвинутых",
+        category : courseCategoryes.html5,
+        duration : 76,
+        rating : 4.5,
+        image : "img/picture-3.svg"
+    },
+    
+    {
+        title : "Node.JS для продвинутых",
+        category : courseCategoryes.nodeJs,
+        duration : 24,
+        rating : 4.7,
+        image : "img/picture-4.svg"
+    },
+    
+    {
+        title : "Angular для продвинутых",
+        category : courseCategoryes.angular,
+        duration : 15,
+        rating : 2.3,
+        image : "img/picture-5.svg"
+    },
+    
+    {
+        title : "Vue JS для продвинутых",
+        category : courseCategoryes.vueJs,
+        duration : 63,
+        rating : 4.1,
+        image : "img/picture-6.svg"
+    },
+    
+    {
+        title : "Django для продвинутых",
+        category : courseCategoryes.django,
+        duration : 46,
+        rating : 4.2,
+        image : "img/picture-1.svg"
+    },
+    
+]
